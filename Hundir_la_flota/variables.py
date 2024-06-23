8d072a585738159ef450505aa5f4d47a7910a10a
somm14 <sorimalpica@gmail.com>
2024-06-23 06:19:39 +0200

    Update variables.py

--- Hundir_la_flota/variables.py
+++ Hundir_la_flota/variables.py
@@ -1,6 +1,6 @@
 from Hundir_la_flota import clases 
 
-player1 = input("Introduzca su nomnnre")
+player1 = input("Introduzca su nombre")
 player2 = "computadora"
 
 vidas1 = 20
@@ -8,11 +8,11 @@ vidas2 = 20
 
 
 # (SORAYA) He creado los tableros dentro de la clase.
-tablero1 = Tablero(10)
+tablero1 = Tablero(10) # type: ignore
 tablero1.tablero_player1(10)
 
 
-tablero2 = Tablero(10)
+tablero2 = Tablero(10) # type: ignore
 tablero2.tablero_player2(10)
 
 '''

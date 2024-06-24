5a75ab46e473ece87814393c9c929f57ffb026a5
somm14 <sorimalpica@gmail.com>
2024-06-24 19:12:25 +0200

    Update clases.py

--- Hundir_la_flota/clases.py
+++ Hundir_la_flota/clases.py
@@ -1,5 +1,4 @@
 import numpy as np
-import variables
 import random
 
 class Tablero():
@@ -38,29 +37,28 @@ class Barco:
                 tablero[pieza] = "O"
         return tablero
     
-    def crea_barco_aleatorio(self, tablero):
-        for barco in barcos_player2:
-            longitud = barco.longitud
-            fila = random.randint(0,9)
-            columna = random.randint(0,9)
-            orientacion = random.choice(["N", "S", "E", "O"])
-            if orientacion == "N": 
-                if fila - longitud >= 0: 
-                    if all(tablero[fila:fila + longitud, columna] == " "):
-                        tablero[fila:fila + longitud, columna] = "O" 
-            if orientacion == "S":
-                if fila + longitud <= len(tablero):
-                    if all(tablero[fila:fila + longitud, columna] == " "):                  
-                        tablero[fila:fila + longitud, columna] = "O"
-            if orientacion == "E":
-                if columna + longitud <= len(tablero):
-                    if all(tablero[fila, columna:columna + longitud] == " "):
-                        tablero[fila, columna:columna + longitud] = "O"
-            if orientacion == "O":
-                if columna  - longitud >= 0:
-                    if all(tablero[fila, columna:columna + longitud] == " "):
-                        tablero[fila, columna:columna + longitud] = "O"
-            return tablero
+    def crea_barco_aleatorio(self, tablero, nombre):
+        longitud = nombre.longitud
+        fila = random.randint(0,9)
+        columna = random.randint(0,9)
+        orientacion = random.choice(["N", "S", "E", "O"])
+        if orientacion == "N": 
+            if fila - longitud >= 0: 
+                if all(tablero[fila:fila + longitud, columna] == " "):
+                    tablero[fila:fila + longitud, columna] = "O" 
+        if orientacion == "S":
+            if fila + longitud <= len(tablero):
+                if all(tablero[fila:fila + longitud, columna] == " "):                  
+                    tablero[fila:fila + longitud, columna] = "O"
+        if orientacion == "E":
+            if columna + longitud <= len(tablero):
+                if all(tablero[fila, columna:columna + longitud] == " "):
+                    tablero[fila, columna:columna + longitud] = "O"
+        if orientacion == "O":
+            if columna  - longitud >= 0:
+                if all(tablero[fila, columna:columna + longitud] == " "):
+                    tablero[fila, columna:columna + longitud] = "O"
+        return tablero
 
 
 
